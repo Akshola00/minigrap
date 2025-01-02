25726a49e414f79cff8a3f3fa86127592f53fc0b
“Akshola00” <akinniyishola07@gmail.com>
2025-01-02 22:38:23 +0100

    refac: modularizing code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-use std::{env, fs};
+use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-    println!("{:?}", args);
 
-    let contents = fs::read_to_string(filename).expect("error trying to read the file");
+    let parsed_data = Data::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // println!("Searching for {}", query);
+    // println!("In file {}", filename);
+    println!("{:?}", args);
+    run(parsed_data);
+
+
+}
+fn run(config: Data) {
+    let contents = fs::read_to_string(config.filename).expect("error trying to read the file");
     println!("With text:\n{}", contents);
 }
 struct Data {
     query: String,
     filename: String,
 }
-fn parse_config(args: &[String]) -> Data {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Data {query, filename}
+
+impl Data { 
+    fn new(args: &[String]) -> Result<Data, &str> {
+        if args.len() != 3 {
+            return Err("expected 3 arguments")
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+       Ok(Data {query, filename})
+    }
 }

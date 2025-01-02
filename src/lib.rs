697676ff73b0bceb2e9ce89a901da08b02a93f5b
“Akshola00” <akinniyishola07@gmail.com>
2025-01-02 23:52:49 +0100

    chore: start implementing search

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+use std::{fs, error::Error};
+
+pub fn run(config: Data) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    print!("With text:\n{}", contents);
+    Ok(())
+}
+
+pub fn search (query: &str, contents: &str) -> Vec<&str> {
+
+}
+pub struct Data {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Data { 
+    pub fn new(args: &[String]) -> Result<Data, &str> {
+        if args.len() != 3 {
+            return Err("expected 3 arguments")
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+       Ok(Data {query, filename})
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_data() {
+        let args = vec![String::from("minigrep"), String::from("query"), String::from("filename")];
+        let data = Data::new(&args).unwrap();
+        assert_eq!(data.query, "query");
+        assert_eq!(data.filename, "filename");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{{env, fs, process}, error::Error};
+use std::env;
+use std::process;
+
+use minigrep::Data;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -10,29 +13,10 @@ fn main() {
     // println!("Searching for {}", query);
     // println!("In file {}", filename);
     println!("{:?}", args);
-    if let Err(e) = run(parsed_data) {
+    if let Err(e) = minigrep::run(parsed_data) {
         println!("Application error: {}", e);
         process::exit(1);
     }
 
 
-}
-fn run(config: Data) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    Ok(())
-}
-struct Data {
-    query: String,
-    filename: String,
-}
-
-impl Data { 
-    fn new(args: &[String]) -> Result<Data, &str> {
-        if args.len() != 3 {
-            return Err("expected 3 arguments")
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-       Ok(Data {query, filename})
-    }
 }

a8fd3b6784fd3cda9195a0b891bb5e282144ff20
“Akshola00” <akinniyishola07@gmail.com>
2025-01-03 01:20:59 +0100

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,12 @@ use std::{fs, error::Error};
 
 pub fn run(config: Data) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    let result = search(&config.query, &contents);
+    let result = if config.case_sensitive {
+        search(&config.query, &contents);
+    } else {
+        search_case_sensitive(&config.query, &contents)
+    };
+
     println!("With text:\n{}\nFound matches:\n{:?}", contents, result);
     Ok(())
 }
@@ -16,19 +21,31 @@ pub fn search<'a> (query: &str, contents: &'a str) -> Vec<&'a str> {
     }
     all_matches
 }
+
+pub fn search_case_sensitive<'a> (query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut all_matches:Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            all_matches.push(line);
+        }
+    }
+    all_matches
+}
 pub struct Data {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Data { 
     pub fn new(args: &[String]) -> Result<Data, &str> {
-        if args.len() != 3 {
+        if args.len() != 4 {
             return Err("expected 3 arguments")
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-       Ok(Data {query, filename})
+        let case_sensitive = args[3].clone();
+       Ok(Data {query, filename, case_sensitive})
     }
 }
 
